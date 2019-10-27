346cd2b24c29e2225ab7e78b86a4060848f46116
Satria Bagus <satria.bagus18@gmail.com>
2019-10-27 08:09:48 +0700

    Add Login Feature and simple Logout using URL

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -10,10 +10,43 @@ class Auth extends CI_Controller{
     }
 
     public function index(){
-        $data['title'] = 'Zeeny Login';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+        
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Zeeny Login';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        }else{
+            $this->_login();
+        }
+    }
+
+    private function _login(){
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user= $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if ($user) {
+            if($user['is_active']== 1){
+            if(password_verify($password, $user['password'])){
+                $data = [
+                    'email' => $user['email'],
+                    'role_id' => $user['role_id']
+                ];
+                $this->session->set_userdata($data);
+                redirect('user');
+            }else{
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong Password !</div>');
+            }
+        }else{
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">User not Activated, Check your MailBox !</div>');
+            }
+        }else{
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email not Registered ! </div>');
+        }
     }
 
     public function register(){
@@ -47,5 +80,14 @@ class Auth extends CI_Controller{
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Account Has Been Created ! Please Login </div>');
             redirect('auth');
         }
+    }
+    
+    public function logout(){
+        
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> You have been Logged Out !</div>');
+        redirect('auth');
+        
     }
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -17,14 +17,16 @@
                                     </div>
                                     <?= $this->session->flashdata('message');
                                     ?>
-                                    <form class="user">
+                                    <form class="user" method="post" action="<?=base_url('auth')?>">
                                         <div class="form-group">
                                             <input type="text" class="form-control form-control-user" id="email" placeholder="Enter Email Address..."
-                                            name="email">
+                                            name="email" value="<?= set_value('email')?>">
+                                            <?= form_error('email', '<small class="text-danger pl-3">', '</small>');?>
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user" id="password" placeholder="Password"
                                             name="password">
+                                            <?= form_error('password', '<small class="text-danger pl-3">', '</small>');?>
                                         </div>
                                         <button type="submit" class="btn btn-primary btn-user btn-block">
                                             Login
@@ -35,7 +37,7 @@
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="<?=base_url('auth/register')?>">Create an Account!</a>
+                                        <a class="small">Create an Account!</a>
                                     </div>
                                 </div>
                             </div>
